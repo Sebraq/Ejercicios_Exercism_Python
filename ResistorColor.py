b543cc1f3c993a0af6ccd317e03370d0433fdf7c
SebasletoswsKy <65198928+Sebraq@users.noreply.github.com>
2023-05-20 06:28:19 -0600

    Add files via upload

---
+++ ResistorColor.py
@@ -0,0 +1,9 @@
+code=['black','brown','red','orange','yellow','green','blue','violet','grey','white']
+def color_code(color):
+    return code.index(color)
+
+#print(color_code('red'))
+def colors():
+    return code
+
+#print(colors())
