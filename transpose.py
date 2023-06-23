ec65c2860f6838eed6d757db01dd1b00630264e9
SebasletoswsKy <65198928+Sebraq@users.noreply.github.com>
2023-06-22 18:01:49 -0600

    Update transpose.py

--- transpose.py
+++ transpose.py
@@ -2,7 +2,6 @@ from itertools import zip_longest
 
 def transpose(lines):
     lines = lines.splitlines()
-    print(lines)
     lines=(l for l in zip_longest(*lines,fillvalue='_'))
     lines = ("".join(l).rstrip("_").replace("_", " ") for l in lines)
     return "\n".join(lines).rstrip()
